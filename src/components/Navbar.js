014114b48db43342f2bd355ae82b7b073ff4710f
moises gomez <moisesg526@yahoo.com>
2023-08-12 22:18:27 -0400

    Added google font and nav buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,15 @@
+import "../styles/Navbar.css";
 function Navbar() {
     return(
         <nav>
-            navbar
+            <div className="logo">HVAC</div>
+            <ul>
+                <li>Home</li>
+                <li>Services</li>
+                <li>About Us</li>
+                <li>Reviews</li>
+                <li>Contact Us</li>
+            </ul>
         </nav>
     )
 }
