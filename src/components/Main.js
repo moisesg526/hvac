c00584e89a6c7c4d30e8428dd49fae41a437d02a
moises gomez <moisesg526@yahoo.com>
2023-08-16 18:15:52 -0400

    Added a main component

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import "../styles/style.css";
+
+function Main() {
+    return(
+        <div className="main-container">
+            <div className="clear-container">
+                <h1>We service all of the NY and NJ area</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Main; 
