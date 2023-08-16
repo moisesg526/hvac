59192ec6fefb33813cb4bf9f02371e4ed5658ddd
moises gomez <moisesg526@yahoo.com>
2023-08-15 22:58:41 -0400

    Added media queries for all screens and adjusted padding to nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,20 @@
 import "../styles/Navbar.css";
 function Navbar() {
-    return(
-        <nav>
-            <div className="logo">HVAC</div>
-            <ul>
-                <li>Home</li>
-                <li>Services</li>
-                <li>About Us</li>
-                <li>Reviews</li>
-                <li>Contact Us</li>
-            </ul>
-        </nav>
-    )
+  return (
+    <nav>
+      <div className="logo">HVAC</div>
+      <ul>
+        <li>Home</li>
+        <li>Services</li>
+        <li>About Us</li>
+        <li>Reviews</li>
+        <li>Contact Us</li>
+      </ul>
+      <div className="dropdown-btn">
+        <i className="fa-solid fa-bars"></i>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
