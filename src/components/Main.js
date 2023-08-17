7b62fb3741aa08b6196d11f2fda6cd92178f5fe4
moises gomez <moisesg526@yahoo.com>
2023-08-16 21:12:01 -0400

    Added the media queries for large screens

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,9 +3,9 @@ import "../styles/style.css";
 function Main() {
     return(
         <div className="main-container">
-            <div className="clear-container">
                 <h1>We service all of the NY and NJ area</h1>
-            </div>
+                <h2>fgikgvnjfiknfredsn</h2>
+                <h3>njsdjchvf</h3>
         </div>
     )
 }
