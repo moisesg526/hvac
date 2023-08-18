1c333a47cc2ed4e781901cdf5f67593c449bca1f
moises gomez <moisesg526@yahoo.com>
2023-08-17 21:56:47 -0400

    Changed the background image and added a schedule button

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,14 @@
 import "../styles/style.css";
 
 function Main() {
-    return(
-        <div className="main-container">
-                <h1>We service all of the NY and NJ area</h1>
-                <h2>fgikgvnjfiknfredsn</h2>
-                <h3>njsdjchvf</h3>
-        </div>
-    )
+  return (
+    <div className="main-container">
+      <div className="main-content">
+        <h1>We service all of the NY and NJ area</h1>
+        <button className="appointment-btn">Schedule and Appointment</button>
+      </div>
+    </div>
+  );
 }
 
-export default Main; 
+export default Main;
