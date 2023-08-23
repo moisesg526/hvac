763693fbbce9be9446f50693e5bd2cc37c1dcdea
moises gomez <moisesg526@yahoo.com>
2023-08-22 23:42:59 -0400

    Added media queries to footer and pitch components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,18 @@ function Footer() {
     <div className="footer-container">
       <div className="left-footer">
         <ul>
-            <li><i className="fa-brands fa-facebook-f"></i></li>
-            <li><i className="fa-brands fa-instagram"></i></li>
-            <li><i className="fa-regular fa-envelope"></i></li>
-            <li><i className="fa-regular fa-envelope"></i></li>
+          <li>
+            <i className="fa-brands fa-facebook-f"></i>
+          </li>
+          <li>
+            <i className="fa-brands fa-instagram"></i>
+          </li>
+          <li>
+            <i class="fa-brands fa-twitter"></i>
+          </li>
+          <li>
+            <i className="fa-regular fa-envelope"></i>
+          </li>
         </ul>
       </div>
       <div className="right-footer">
