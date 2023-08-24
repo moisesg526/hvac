3c7702c4d7a71d56e3ee72e9bb50e2c9392f54c9
moises gomez <moisesg526@yahoo.com>
2023-08-23 23:35:11 -0400

    Added scss file for whyus component

--- src/components/Whyus.js
+++ src/components/Whyus.js
@@ -1,18 +1,29 @@
-function Whyus() {
-    return (
-        <div className="whyus-container">
-            <h3>Why choose HVAC?</h3>
-            <div className="row1">
-                <div className="reasons">Licensed and HVAC ceritfied</div>
-                <div className="reasons">We beat our competitors estimates!</div>
-                <div className="reasons">Manufacturers Warranties</div>
-            </div>
-            <div className="row2">
-                <div className="reasons">Quick and effient response.</div>
-                <div className="reasons">Years of experience with many happy customers</div>
-                <div className="reasons">Reasonable payment plans!</div></div>
-        </div>
-    )
-}
+import "../styles/style.css";
 
+function Whyus() {
+  return (
+    <div className="whyus-container">
+      <h3>Why choose HVAC?</h3>
+      <div className="row1">
+        <div className="reasons">
+          <i class="fa-solid fa-award why-icon"></i> Licensed and HVAC ceritfied
+        </div>
+        <div className="reasons">
+          <i class="fa-solid fa-trophy"></i> We beat our competitors estimates!
+        </div>
+        <div className="reasons"><i class="fa-solid fa-shield-halved"></i> Manufacturers Warranties</div>
+      </div>
+      <div className="row2">
+        <div className="reasons"><i class="fa-solid fa-clock"></i> Quick and effient response.</div>
+        <div className="reasons">
+          <i class="fa-solid fa-shield-heart"></i>
+          Years of experience with many happy customers
+        </div>
+        <div className="reasons">
+          <i class="fa-solid fa-receipt"></i> Reasonable payment plans!
+        </div>
+      </div>
+    </div>
+  );
+}
 export default Whyus;
