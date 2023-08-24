9fc4453f92c72401c1a6fa92363334e2be23e38c
moises gomez <moisesg526@yahoo.com>
2023-08-24 00:37:41 -0400

    Added icons and padding to whyus component

--- src/components/Whyus.js
+++ src/components/Whyus.js
@@ -4,23 +4,33 @@ function Whyus() {
   return (
     <div className="whyus-container">
       <h3>Why choose HVAC?</h3>
-      <div className="row1">
+      <div className="row">
         <div className="reasons">
-          <i class="fa-solid fa-award why-icon"></i> Licensed and HVAC ceritfied
+          <i className="fa-solid fa-award why-icon"></i> <br />
+          Licensed and HVAC ceritfied
         </div>
         <div className="reasons">
-          <i class="fa-solid fa-trophy"></i> We beat our competitors estimates!
+          <i className="fa-solid fa-trophy why-icon"></i> <br />
+          We beat our competitors estimates!
         </div>
-        <div className="reasons"><i class="fa-solid fa-shield-halved"></i> Manufacturers Warranties</div>
+        <div className="reasons">
+          <i className="fa-solid fa-shield-halved why-icon"></i>
+          <br /> Manufacturers Warranties
+        </div>
       </div>
-      <div className="row2">
-        <div className="reasons"><i class="fa-solid fa-clock"></i> Quick and effient response.</div>
+      <div className="row">
         <div className="reasons">
-          <i class="fa-solid fa-shield-heart"></i>
+          <i className="fa-solid fa-clock why-icon"></i>
+          <br /> Quick and effient response.
+        </div>
+        <div className="reasons">
+          <i className="fa-solid fa-shield-heart why-icon"></i>
+          <br />
           Years of experience with many happy customers
         </div>
         <div className="reasons">
-          <i class="fa-solid fa-receipt"></i> Reasonable payment plans!
+          <i className="fa-solid fa-receipt why-icon"></i> <br />
+          Reasonable payment plans!
         </div>
       </div>
     </div>
