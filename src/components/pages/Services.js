3867a19ac2b2804c766f358ade824845c79147fa
moises gomez <moisesg526@yahoo.com>
2023-10-11 00:12:44 -0400

    Added some images for service page

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,45 +1,8 @@
-import "/Users/moisesg526/Desktop/hvac/src/styles/style.css";
-
+import Service from "../Service";
 function Services() {
   return (
-    <div className="services-block">
-      <div className="services-heading">
-        <h1>Services</h1>
-        <h3>Trusted Work with reasonalble prices</h3>
-      </div>
-      <div className="services-provided">
-        <div className="service">
-          <img src="" alt="air conditioning" />
-          <h3>Air Conditioning</h3>
-          <h4>
-            We provide services for commercial properties or for your home. We
-            can maintain, repair or replace your existing one. We work on all
-            types of units ductless, central AC and rooftops
-          </h4>
-        </div>
-        <div className="service">
-          <img src="" alt="Heating" />
-          <h3>Heating</h3>
-          <h4>
-            We service, maintain and replace all types of heating systems. We
-            can get your place heated and warm as quick and effienct as
-            possible. Whether its a furnace, boilers, radiant heating, or forced
-            hot air our services provided will be top notch.
-          </h4>
-        </div>
-        <div className="service">
-          <img src="" alt="Water Heaters" />
-          <h3>Water Heaters</h3>
-          <h4>
-            We service and replace all types of water heaters includint gas,
-            electric tanks, tankless, and heat pump water heaters. We are
-            trained and provide multiple years of skills and experience to
-            satisfy your needs.
-          </h4>
-        </div>
-      </div>
-    </div>
-  );
+    <Service />
+  )
 }
 
 export default Services;
